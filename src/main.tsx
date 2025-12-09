fd74c547d43805857e9f6f1a56bdb18207c9e94c
Solomon Olatunji <aotoluwalope@gmail.com>
2025-12-09 11:11:47 +0100

    feat: add sonner Toaster component for displaying notifications

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <Router>
+          <Toaster position="top-center" richColors />
           <App />
         </Router>
       </QueryClientProvider>
